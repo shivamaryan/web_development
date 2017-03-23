1e471022ccf7696a646f8359acdb15c68e410e31
Shantanu Garg <shivam.aryan17@gmail.com>
2017-03-24 02:29:13 +0530

    using render, the built in library instead of HttpResponse

--- Programmers_Square/music/views.py
+++ Programmers_Square/music/views.py
@@ -1,15 +1,13 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.template import loader
 from django.http import HttpResponse
 from .models import Album
 
 def index(request):
     all_albums=Album.objects.all()
-    template =loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'all_albums': all_albums}
+
+    return render(request, 'music/index.html', context)         #HttpRequest is actually being sent in backg. owing to built in
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id)+"</h2>")
