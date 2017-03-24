5b0bf8ae4c775ef484ca7c7a67c61eb0432c9091
Shantanu Garg <shivam.aryan17@gmail.com>
2017-03-24 10:22:48 +0530

    After beautiful images appear and Albums display correctly

--- Programmers_Square/music/views.py
+++ Programmers_Square/music/views.py
@@ -1,13 +1,14 @@
 from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Album
 
 def index(request):
     all_albums=Album.objects.all()
-    context = {'all_albums': all_albums}
-
-    return render(request, 'music/index.html', context)         #HttpRequest is actually being sent in backg. owing to built in
+    return render(request, 'music/index.html', {'all_albums': all_albums})         #HttpRequest is actually being sent in backg. owing to built in
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id)+"</h2>")
+    try:
+        album=Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album} ) #using context would raise local varialbe issues
