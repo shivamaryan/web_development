45d54d7a09a6b75fe1bd8740f33bcbbf41c4cfaa
Shantanu Garg <shivam.aryan17@gmail.com>
2017-04-06 12:30:07 +0530

    After Successfully creating forms, using radio buttons and marking favourites

--- Programmers_Square/music/urls.py
+++ Programmers_Square/music/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
 
     # /music/71,    ie album with id 71
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+# /music/<album_id>/favourite    ie album with id 71
+   url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
